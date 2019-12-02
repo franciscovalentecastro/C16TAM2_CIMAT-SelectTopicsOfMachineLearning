9d4e186519e85a11ac8eba06ab3e3e984be46dd8
Francisco Valente <fco.valente.castro@gmail.com>
2019-12-01 18:21:59 -0600

    ADd

--- resnet-posture/network.py
+++ resnet-posture/network.py
@@ -9,23 +9,29 @@ class Resnet_Posture(nn.Module):
     def __init__(self, args):
         super(Resnet_Posture, self).__init__()
 
+        # Parameters
+        self.image_shape = args.image_shape
+
         # Classifier input dimension
         self.lin_inpt = 512 * (args.image_shape[0] // 32) * \
             (args.image_shape[1] // 32)
 
-        self.resnet = models.resnet152(pretrained=True)
+        self.resnet = models.resnet18(pretrained=True)
 
         self.linear = nn.Sequential(nn.Linear(self.lin_inpt,
                                               self.lin_inpt // 2),
                                     nn.ReLU(),
                                     nn.Linear(self.lin_inpt // 2,
-                                              (5 * self.bboxes + 4) * 7 * 7),
-                                    nn.Sigmoid())
+                                              10 *
+                                              self.image_shape[0] *
+                                              self.image_shape[1]))
 
     def forward(self, x):
         x1 = self.resnet.features(x)
         x1 = x1.view(-1, self.lin_inpt)
         x2 = self.linear(x1)
-        y = x2.view([-1, (5 * self.bboxes + 4), 7, 7])
+        y = x2.view([-1, 10,
+                     self.image_shape[0],
+                     self.image_shape[1]])
 
         return y
