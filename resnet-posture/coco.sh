d8bbf55dc23854ef2b61716a76525561f1f280fb
Francisco Valente <fco.valente.castro@gmail.com>
2019-12-03 00:43:36 -0600

    coco sh

--- resnet-posture/coco.sh
+++ resnet-posture/coco.sh
@@ -8,10 +8,10 @@ wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
 wget http://images.cocodataset.org/zips/unlabeled2017.zip
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
-unzip unlabeled2017.zip
+unzip train2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip val2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip test2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip unlabeled2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
 
 rm train2017.zip
 rm val2017.zip
@@ -24,10 +24,10 @@ wget http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zi
 wget http://images.cocodataset.org/annotations/image_info_test2017.zip
 wget http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
 
-unzip annotations_trainval2017.zip
-unzip stuff_annotations_trainval2017.zip
-unzip image_info_test2017.zip
-unzip image_info_unlabeled2017.zip
+unzip annotations_trainval2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip stuff_annotations_trainval2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip image_info_test2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip image_info_unlabeled2017.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
 
 rm annotations_trainval2017.zip
 rm stuff_annotations_trainval2017.zip
